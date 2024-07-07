20fde69caf77be99b8dc24d644ba747477d63688
sanasatsangi14 <sana.satsangi14@gmail.com>
2024-07-07 23:17:06 +0530

    frontend done

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Home from "./Home/page";
+//import Course from './components/Course';
+import {Route,Routes} from "react-router-dom"
+import Courses from './courses/Courses';
+import Signup from './components/Signup';
+
+
+function App(){
+  return (
+    <>
+      {/*<Home />
+      <Course/>*/}
+      <Routes>
+        <Route path="/" element={<Home />}/>
+    
+        <Route path="/course" element={<Courses/>}/>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/Home/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Banner from '../components/Banner'
+import Free from '../components/Free'
+import Footer from '../components/Footer'
+
+function page() {
+  return (
+    <>
+    <Navbar/>
+      <Banner/>
+      <Free/>
+     <Footer/>
+      </>
+  )
+}
+
+export default page
